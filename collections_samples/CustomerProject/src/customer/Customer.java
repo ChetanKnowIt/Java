f74ee8e48fa6c3db2ceb123ea8073406c7ae9354
ChetanKnowIt <chetanamrao174@gmail.com>
2022-12-21 20:31:11 +0530

    collections added

---
+++ collections_samples/CustomerProject/src/customer/Customer.java
@@ -0,0 +1,24 @@
+package customer;
+
+import java.util.*;
+
+
+public class Customer {
+
+	public static void main(String[] args) {
+		
+		/* name
+		 * email
+		 * contactno
+		 * accept 10 customers 
+		 * write to file with RegisteredCustomer(regno)
+		 * 
+		 * 
+		 * */
+
+		
+		
+		
+	}
+
+}
