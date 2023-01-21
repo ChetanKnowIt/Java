d5b83fe5fd5a64979a391a0095e77481cbbc39de
ChetanKnowIt <chetanamrao174@gmail.com>
2023-01-21 18:15:40 +0530

    End module questions

---
+++ StartWithUpperCase.java
@@ -0,0 +1,35 @@
+public class StartWithUpperCase{
+
+        public static void main(String[] args){
+	//String mystr = "For our case, we need a pattern";
+	try{
+		if(args.length>1)
+		{
+			for(int i=0;i<args.length;i++)
+			{
+				System.out.println(toggleCases(args[i]));
+			}
+		}
+		else
+			System.out.println("Insufficient Arguments");
+	}
+
+	catch(Exception e){
+		e.printStackTrace();
+	}
+        }
+
+        public static String toggleCases(String str){
+            char[] arr = new char[str.length()];
+            
+            for(int i=0;i<str.length();i++){
+                char ch=str.charAt(i);
+                if(ch>=65 && ch<=90)
+                    ch=(char)(ch+32);
+                else if(ch>=97 && ch<=122)
+                    ch=(char)(ch-32);
+                arr[i] = ch;
+                }
+            return new String(arr,0, arr.length);
+    }
+}
